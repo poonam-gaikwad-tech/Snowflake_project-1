ee22100741742afde9f9a59b20ad014a8ecf5522
poonam-gaikwad-tech <poonam.gaikwad.tech@gmail.com>
2025-09-18 12:31:33 +0200

    Update streamlit_app.py
    set width to strech

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if indgredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),width="strech")
